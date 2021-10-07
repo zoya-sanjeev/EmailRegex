23526089e3c01e0b2a7225fde6cde7c5b988811e
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-10-07 22:31:27 +0530

    ADDED:email validation for mandatory part abc

--- EmailValidation.js
+++ EmailValidation.js
@@ -1,1 +1,17 @@
 console.log("Welcome to email validation");
+
+let emailRegex = RegExp("^abc$");
+
+function validateEmail(email){
+    if(emailRegex.test(email))
+        console.log("Valid Email");
+    else
+        throw 'Invalid email';
+}
+
+try{
+    validateEmail("abc");
+    validateEmail("qwer");
+}catch(e){
+    console.error(e);
+}
